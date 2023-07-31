18539b3dbc46f1690ee3d7e50e213c6d3ff70543
sunho Yum <99744822+SgYumsunho@users.noreply.github.com>
2023-07-31 16:35:06 +0900

    [Silver III] Title: 교수가 된 현우, Time: 4720 ms, Memory: 116708 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.py
@@ -0,0 +1,12 @@
+def find_fives(n) :
+    t = 5
+    cnt = 0
+    while t<= n:
+        cnt += n//t
+        t*=5
+    return cnt
+
+n = int(input())
+for i in range(n) :
+    t = int(input())
+    print(find_fives(t))
